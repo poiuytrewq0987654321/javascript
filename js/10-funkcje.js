0a346ba11ce17a0f3be0e4f65d29d4b155606786
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-06-10 15:08:54 +0200

    funkcje

---
+++ js/10-funkcje.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*function multiply(parametr1, parametr2, parametr3) {
+    var result;
+    result = parametr1 * parametr2 * parametr3;
+    return result;
+}
+
+var wynik = multiply(3,9,10);
+wynik = multiply(23,24,3);
+
+console.log("wyniki: " + wynik);*/
+
+var funkcja = function () {
+    return "wynik :)";
+};
+console.log(funkcja);
+console.log(funkcja () );
+
+
