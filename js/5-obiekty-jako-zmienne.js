7f9b61a995d67fe1a097e1977e50bafb873be0c7
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-06-10 11:55:00 +0200

    obiekty
    obiekty jako zmienne; obiekty Math

---
+++ js/5-obiekty-jako-zmienne.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* obiekty jako zmienne */
+
+var student = {
+    imie: "Kaja",
+    wzrost:152,
+    wyswietlDane: function() { console.log(this.imie); }
+}
+
+student.wyswietlDane();
+
+console.log(student);
---
+++ js/6-obiekt-math.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log( Math.PI );
+console.log( Math.cos(0) );
+
+/* pole koła PI * r do kwadratu */
+var r = 10;
+console.log(Math.PI * Math.pow(r,2));
+
+
+alert(Math.PI * Math.pow(r,2));
+
