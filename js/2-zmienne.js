fbaa7ce511561ff2fc6c399e02a072979fcb9d87
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-06-10 09:53:27 +0200

    zmienne
    omowienie zmiennych

---
+++ js/2-zmienne.js
@@ -0,0 +1,24 @@
+'use strict';
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor = "czerwony";
+
+imie = "Sergio";
+
+imie = imieDamskie;
+
+console.log(imie);
+console.log(imieDamskie);
+console.log(kolor);
+console.log(wiek);
+
+
+
+
+
+
+
+
+
