8bd0d1144420d93a739bc2e649fddde8049bb41a
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-06-10 14:35:39 +0200

    operatory

---
+++ js/9-operatory.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/*var licz = 7;
+licz += 3;
+// można to również zapisać jako: licz = licz + 3;
+console.log(licz);
+
+var liczba1 = "13", liczba2 = 13;
+if(liczba1 == liczba2) {
+    console.log("równe");
+}
+var wynik;
+wynik = liczba1 % liczba2;*/
+
+/*var liczba1 = 13, liczba2 = 4;
+if( (liczba2 >3) && (liczba2 <10)){
+    console.log("tak");
+}*/
+
+
+(7>5) ? console.log("tak") : console.log("nie");
+//skrócony zapis if'a poniżej
+if(7>5){
+    console.log("tak");
+}else{
+    console.log("nie");
+}
+
+
+
